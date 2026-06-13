fc275c24c692f48341d92c5934f67c41528215f0
akatoyo <oscarbotina@hotmail.com>
2026-06-13 00:05:51 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Cliente único de Supabase con tipado explícito para habilitar el autocompletado
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey)
